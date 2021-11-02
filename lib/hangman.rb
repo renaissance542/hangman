80c5fedd5efcd3230c536c5b9a7dcd48f6af640c
Philippe Roy <renaissance542@gmail.com>
2021-11-02 19:20:23 -0400

    update secret word methods
    mystery_word displays the letters correctly guessed and blanks for the rest
    generate_secret_word reads the dictionary file and removes words that are too short or too long

---
+++ lib/hangman.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# main menu: load game, play game
+# load game:  read JSON file and create game object
+# play game: (option loaded game parameter)
+  # load dictionary
+  # display message
+    # show correct guesses in word
+    # show incorrect guesses
+    # show prompt:
+      # 1 guess letter
+      # 2 guess word
+      # 3 save and quit
+  # guess letter
+    # update guess so far
+    # win or lose
+    # next guess
+  # save game
+
+  
